9637df84a8161fd35ae3278c62b18b3d29e953b9
ChanYenFen <yfenchan@gmail.com>
2026-04-18 14:53:45 +0800

    remove correction_legacy and clean cache files

--- src/geometry/correction/__init__.py
+++ src/geometry/correction/__init__.py
@@ -3,16 +3,3 @@ from .span import interpolate_modified_spans, detect_core_spans, expand_neighbor
 from .smooth import smooth_pull_magnitude_field, smooth_core_displacement
 from .decay import apply_decayed_pull
 from .refit import refit_modified_spans
-# from geometry.correction_legacy import (
-#     smooth_pull_magnitude_field,
-#     smooth_core_displacement,
-#     refit_modified_spans,
-#     apply_decayed_pull,
-# )
-"""
-from .pull import pull_points
-from .span import interpolate_modified_spans, detect_core_spans, expand_neighborhood
-from .smooth import smooth_pull_magnitude_field, smooth_core_displacement
-from .decay import apply_decayed_pull
-from .refit import refit_modified_spans
-"""
